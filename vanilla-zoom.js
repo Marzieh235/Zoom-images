c792c227602dc671221075f35e6491871fca6a98
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-06 17:16:34 +0330

    Create package structure

---
+++ vanilla-zoom.js
@@ -0,0 +1,12 @@
+(function (window) {
+    let defineLibrary = () => ({
+        init : 
+    })
+
+    if (typeof (vanillaZoom) == 'undefind') {
+        window.vanillaZoom = defineLibrary();
+    } else {
+        console.log('Library already defined.')
+    }
+
+})(window)
