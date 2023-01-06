ff4d33afae07b79db4255af8e759b62be41e5bce
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-06 17:57:40 +0330

    Change the zoomed image

--- vanilla-zoom.js
+++ vanilla-zoom.js
@@ -1,9 +1,47 @@
 (function (window) {
     let defineLibrary = () => ({
-        init : 
+        init: function (galleryId) {
+            let container = document.querySelector(galleryId);
+
+            if (!container) {
+                console.error('please add the correct element');
+                return;
+            }
+
+
+
+
+            let firstImage = container.querySelector('.small-preview');
+            let zoomImage = container.querySelector('.zoomed-image');
+
+
+            if (!zoomImage) {
+                console.error('please add a .zoomed-image tag');
+                return;
+            }
+
+            if (!firstImage) {
+                console.error('please add images with .small-preview class to your gallery');
+                return;
+            }
+
+            zoomImage.style.backgroundImage = `url(${firstImage.src})`
+
+
+            container.addEventListener('click', function (e) {
+                let elem = e.target;
+
+                if (elem.classList.contains('small-preview')) {
+                    zoomImage.style.backgroundImage = `url(${elem.src})`
+
+                }
+
+            })
+
+        }
     })
 
-    if (typeof (vanillaZoom) == 'undefind') {
+    if (typeof (vanillaZoom) == 'undefined') {
         window.vanillaZoom = defineLibrary();
     } else {
         console.log('Library already defined.')
